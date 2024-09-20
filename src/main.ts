522dae114389b7562c4bef37fa8db5a4bb5b60ac
Bubblegum <a82727282@gmail.com>
2024-09-20 18:09:17 +0900

    update readme

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,11 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://ggair.co.kr', 'http://localhost:3000'],
+    origin: [
+      'https://www.ggair.co.kr',
+      'https://www.api.ggair.co.kr',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
