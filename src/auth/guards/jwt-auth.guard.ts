2b54a7a836a25c9a31b315696f970a7070679432
Bubblegum <a82727282@gmail.com>
2024-09-20 15:06:24 +0900

    update cookie and authorization action

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,31 +1,34 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
-  canActivate(
-    context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext) {
     try {
       const request = context.switchToHttp().getRequest();
       const authCookie = request.cookies['authorization'];
       if (!authCookie) {
-        return false;
+        throw new UnauthorizedException('인증 토큰이 없습니다.');
       }
 
       const [type, token] = authCookie.split(' ');
       if (type !== 'Bearer') {
-        return false;
+        throw new BadRequestException('인증 형식이 일치하지 않습니다.');
       }
       const user = this.jwtService.verify(token);
       request.user = user;
 
       return true;
-    } catch (e) {
-      return false;
+    } catch (error) {
+      throw error;
     }
   }
 }
