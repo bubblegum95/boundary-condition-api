1ea4117f67c78452ad25d5397c9d3a3e1f34e747
Bubblegum <a82727282@gmail.com>
2024-09-20 18:33:54 +0900

    update app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,9 +6,4 @@ import { LoggingInterceptor } from './loggingInterceptor';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
