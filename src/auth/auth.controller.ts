35c4a7c43de3ca6b703a791b8d0ac9cdda22cc68
Bubblegum <a82727282@gmail.com>
2024-09-23 13:00:50 +0900

    update cookie set

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -28,7 +28,7 @@ export class AuthController {
       const token = await this.authService.signInAdmin(dto);
       res.cookie('authorization', `Bearer ${token}`, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none',
       });
 
